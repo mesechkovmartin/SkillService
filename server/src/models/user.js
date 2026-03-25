35917f9457f092790f7b2f2a1825598b16cf6ee1
Martin Mesechkov <mesechkov.martin@gmail.com>
2026-03-25 17:23:44 +0200

    feat: update user schema to include profilePicture, city, and biography fields with default values

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -3,11 +3,14 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     firebaseUid: { type: String, unique: true, sparse: true },
-    email: { type: String, required: true, unique: true },
-    username: { type: String },
-    firstName: { type: String },
-    lastName: { type: String },
-    phoneNumber: { type: String },
+    email: { type: String, required: true, unique: true, trim: true },
+    username: { type: String, trim: true },
+    firstName: { type: String, trim: true },
+    lastName: { type: String, trim: true },
+    phoneNumber: { type: String, trim: true },
+    profilePicture: { type: String, default: "" },
+    city: { type: String, trim: true },
+    biography: { type: String, default: "Add bio" },
 
     isAdmin: { type: Boolean, default: false },
     isBlocked: { type: Boolean, default: false },
